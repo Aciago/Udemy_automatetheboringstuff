7ac060d40da5626dfd69e0e120b1e61e760c1a2b
Aciago <aciagomk@gmail.com>
2021-05-03 21:28:08 +0200

    flow control intro

---
+++ flow_control.py
@@ -0,0 +1,48 @@
+# comparison operators
+
+# operator # meaning
+
+42 == 42 # == equal to
+42 == 41
+
+42 != 42 # not equal to
+
+3 < 4 # less than
+
+10 > 0 # greater than
+
+3 <= 3 # less than or equal to
+
+5 => 4 # greater than or equal to
+
+
+
+
+# AND operator's Truth table
+
+# expression # evaluates to
+True and True # True
+
+True and False # False
+
+False and True # False
+
+False and False # False
+
+# OR operator's Truth table
+
+True or True # True
+
+True or False # True
+
+False or True # True
+
+False or False # False
+
+# NOT operator
+
+# evaluates to the opposite value
+
+not True #False
+
+not False #True
--- hello.py
+++ hello.py
@@ -1,11 +1,27 @@
 # this program says hello and asks for my name
 
-print('hello world!')
-print('what is your name?') #ask for users name
+print(
+    'hello world!'
+    )
+
+print(
+    'what is your name?'
+    ) #ask for users name
 myname = input()
-print('it is good to meet you, ' + myname)
-print('the length of your name is:')
-print(len(myname))
-print('what is your age?') #ask for users age
+print(
+    'it is good to meet you, ' + myname
+    )
+print(
+    'the length of your name is:'
+    )
+print(
+    len(myname)
+    )
+
+print(
+    'what is your age?'
+    ) #ask for users age
 myage = input()
-print('you will be ' + str(int(myage) + 1) + ' in a year')
+print(
+    'you will be ' + str(int(myage) + 1) + ' in a year'
+    )
