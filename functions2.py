caf2277caabf824e2ee624cd7d8c5708f3fc52b8
Aciago <aciagomk@gmail.com>
2021-05-06 22:36:44 +0200

    v4

---
+++ functions2.py
@@ -0,0 +1,16 @@
+"""
+def hello():
+    print('wololo')
+    print('wololo!!!')
+    print('WOLOLOLO!!!')
+
+hello()
+hello()
+hello()
+"""
+
+def plusone(number):
+    return number + 1
+
+newnumber = plusone(5)
+print(newnumber)
