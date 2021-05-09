41e6a9f5543b5f8fccb7333b280d8dbb95d1135d
Aciago <aciagomk@gmail.com>
2021-05-09 17:04:32 +0200

    v6

---
+++ lists.py
@@ -0,0 +1,10 @@
+"""
+spam = ['cat', 'dog', 'rat', 'elephant']
+
+supplies = ['pens','staplers','flame-throwers','binders']
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+"""
+
+cat = ['fat', 'orange', 'loud']
+size, color, disposition = cat
