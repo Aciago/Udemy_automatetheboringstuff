7d2e01e63420d731fd8ed2b9ddcd6ed8722101f5
Aciago <aciagomk@gmail.com>
2021-05-15 16:34:58 +0200

    v8

---
+++ strings_formatting.py
@@ -0,0 +1,10 @@
+# string formatting
+
+name = "Alice"
+place = "Main Street"
+time = "6 pm"
+food = "chips"
+
+print('Hello ' + name + ', you are invited to a party at ' + place + ' at ' + time + '. Please bring ' + food + '.')
+
+print('Hello %s, you are invited to a party at %s at %s. Please bring %s.' % (name, place, time, food))
